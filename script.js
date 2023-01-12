61c0e64619b40b59e769b4bb419bc26739e98ec0
Nishchay <nishchayveer19@gmail.com>
2023-01-12 10:56:26 +0530

    ui modified

--- script.js
+++ script.js
@@ -1,68 +1,99 @@
-// https://superheroapi.com/api/access-token/character-id
-
 const SUPERHERO_TOKEN = '10223569763528853'
 const BASE_URL = `https://superheroapi.com/api.php/${SUPERHERO_TOKEN}`
 
-const newHeroButton = document.getElementById('newHeroButton')
+const newHeroButton = document.getElementById('random');
 
-const heroImageDiv = document.getElementById('heroImage')
+const searchButton = document.getElementById('search-btn')
 
-const searchButton = document.getElementById('searchButton')
+const searchInput = document.getElementById('form-control')
 
-const searchInput = document.getElementById('searchInput')
+
+const showSuperheroDetails = (data) => {
+    console.log(data);
+    document.querySelector('.app-body-content-thumbnail').innerHTML = `
+        <img src = "${data.image.url}">
+    `;
+
+    document.querySelector('.name').textContent = data.name;
+    document.querySelector('.powerstats').innerHTML = `
+    <li>
+        <div>
+            <i class = "fa-solid fa-shield-halved"></i>
+            <span>intelligence</span>
+        </div>
+        <span>${data.powerstats.intelligence}</span>
+    </li>
+    <li>
+        <div>
+            <i class = "fa-solid fa-shield-halved"></i>
+            <span>strength</span>
+        </div>
+        <span>${data.powerstats.strength}</span>
+    </li>
+    <li>
+        <div>
+            <i class = "fa-solid fa-shield-halved"></i>
+            <span>speed</span>
+        </div>
+        <span>${data.powerstats.speed}</span>
+    </li>
+    <li>
+        <div>
+            <i class = "fa-solid fa-shield-halved"></i>
+            <span>durability</span>
+        </div>
+        <span>${data.powerstats.durability}</span>
+    </li>
+    <li>
+        <div>
+            <i class = "fa-solid fa-shield-halved"></i>
+            <span>power</span>
+        </div>
+        <span>${data.powerstats.power}</span>
+    </li>
+    <li>
+        <div>
+            <i class = "fa-solid fa-shield-halved"></i>
+            <span>combat</span>
+        </div>
+        <span>${data.powerstats.combat}</span>
+    </li>
+    `;
+}
 
 const getSuperHero = (id, name) => {
-  // name 👉 base_url/search/batman
-  // json.results[0].image.url
-  // id: 👉 base_url/id
-  // json.image.url
-  fetch(`${BASE_URL}/${id}`)
-    .then(response => response.json())
-    .then(json => {
-      console.log(json.powerstats)
-      const superHero = json
-      showHeroInfo(superHero) 
-    })
+    // name 👉 base_url/search/batman
+    // json.results[0].image.url
+    // id: 👉 base_url/id
+    // json.image.url
+    fetch(`${BASE_URL}/${id}`)
+        .then(response => response.json())
+        .then(json => {
+            console.log(json.powerstats)
+            const superHero = json
+            showSuperheroDetails(superHero)
+        })
 }
 
-const statToEmoji = {
-  intelligence: '🧠',
-  strength: '💪',
-  speed: '⚡',
-  durability: '🏋️‍♂️',
-  power: '📊',
-  combat: '⚔️',
-}
-
-const showHeroInfo = (character) => {
-  const name = `<h2>${character.name}</h2>`
-
-  const img = `<img src="${character.image.url}" height=200 width=200/>`
-  
-  const stats = Object.keys(character.powerstats).map(stat => {
-    return `<p>${statToEmoji[stat]} ${stat.toUpperCase()}: ${character.powerstats[stat]}</p>`
-  }).join('')
-  
-  heroImageDiv.innerHTML = `${name}${img}${stats}`
-}
-
-// <p>💪 Strength: ${json.powerstats.strength}</p><p>🧠 Intelligence: ${json.powerstats.intelligence}</p><p>🧠 Combat: ${json.powerstats.intelligence}</p><p>🧠 Speed: ${json.powerstats.intelligence}</p><p>🧠 Durability: ${json.powerstats.intelligence}</p>
 
 const getSearchSuperHero = (name) => {
-  console.log(searchInput.value)
-  fetch(`${BASE_URL}/search/${name}`)
-    .then(response => response.json())
-    .then(json => {
-      const hero = json.results[0]
-      showHeroInfo(hero) 
-    })
-}
+    console.log(searchInput.value)
+    searchInput.value=""
+    fetch(`${BASE_URL}/search/${name}`)
+      .then(response => response.json())
+      .then(json => {
+        const hero = json.results[0]
+        console.log(hero)
+        showSuperheroDetails(hero) 
+      })
+  }
 
 const randomHero = () => {
-  const numberOfHeroes = 731
-  return Math.floor(Math.random() * numberOfHeroes) + 1
+    const numberOfHeroes = 731
+    return Math.floor(Math.random() * numberOfHeroes) + 1
 }
+
+searchButton.onclick = () => getSearchSuperHero(searchInput.value)
 
 newHeroButton.onclick = () => getSuperHero(randomHero())
 
-searchButton.onclick = () => getSearchSuperHero(searchInput.value)
